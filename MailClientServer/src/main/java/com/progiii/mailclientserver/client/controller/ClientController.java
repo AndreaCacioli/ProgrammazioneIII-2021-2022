37e42d5149de5967369672b57a80fb2627670f77
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-11-24 16:01:34 +0100

    Refactor code and still trying to find source of index out of bounds

--- MailClientServer/src/main/java/com/progiii/mailclientserver/client/controller/ClientController.java
+++ MailClientServer/src/main/java/com/progiii/mailclientserver/client/controller/ClientController.java
@@ -2,25 +2,17 @@ package com.progiii.mailclientserver.client.controller;
 
 import com.progiii.mailclientserver.client.model.Client;
 import com.progiii.mailclientserver.client.model.Email;
+import com.progiii.mailclientserver.client.model.EmailState;
 import javafx.beans.binding.Bindings;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 
 public class ClientController {
-    @FXML
-    private Button inboxButtonId;
-    @FXML
-    private Button sentButtonId;
-    @FXML
-    private Button draftsButtonId;
-    @FXML
-    private Button forwardButtonId;
-    @FXML
-    private Button deleteButtonId;
+
     @FXML
     private ListView<Email> emailListView;
     @FXML
@@ -44,32 +36,45 @@ public class ClientController {
     @FXML
     private void showInbox() {
         //TODO: fetch all mails and show them in the listView
+        if(client.inbox.size() > 0) client.selectedEmail = client.inbox.get(0);
         Bindings.bindContent(emailListView.getItems(), client.inbox);
+        bindMailToView(client.selectedEmail);
     }
 
     @FXML
     private void showSent() {
+        if(client.sent.size() > 0) client.selectedEmail = client.sent.get(0);
         //TODO: fetch all sent emails and show them in the listView
         Bindings.bindContent(emailListView.getItems(), client.sent);
+        bindMailToView(client.selectedEmail);
     }
 
     @FXML
     private void showDrafts() {
+        if(client.drafts.size() > 0) client.selectedEmail = client.drafts.get(0);
         //TODO: fetch all drafts mails and show them in the listView
         Bindings.bindContent(emailListView.getItems(), client.drafts);
+        bindMailToView(client.selectedEmail);
     }
 
     //TODO find out why this button causes the others to fail when deleting an email
     @FXML
     private void showTrash() {
+        if(client.trash.size() > 0) client.selectedEmail = client.trash.get(0);
         //TODO: fetch all trashed mails and show them in the listView
         Bindings.bindContent(emailListView.getItems(), client.trash);
+        bindMailToView(client.selectedEmail);
     }
 
     @FXML
-    private void showSelectedEmail() {
+    private void onListViewClick() {
         Email email = emailListView.getSelectionModel().getSelectedItems().get(0);
+        System.out.println(emailListView.getSelectionModel().getSelectedItems());
         client.selectedEmail = email;
+        bindMailToView(email);
+    }
+
+    private void bindMailToView(Email email) {
         fromTextField.textProperty().bind(email.senderProperty());
         toTextField.textProperty().bind(email.receiverProperty());
         subjectTextField.textProperty().bind(email.subjectProperty());
@@ -82,27 +87,53 @@ public class ClientController {
         if (client.selectedEmail != null) {
             switch (client.selectedEmail.state) {
                 case DRAFTED -> {
-                    client.drafts.remove(client.selectedEmail);
-                    client.trash.add(client.selectedEmail);
-                    resetSelectedEmail();
-                    client.selectedEmail = null;
+                    int index = sendSelectedEmailToTrash(client.drafts);
+                    showNextEmail(index);
                 }
 
                 case SENT -> {
-                    client.sent.remove(client.selectedEmail);
-                    client.trash.add(client.selectedEmail);
-                    resetSelectedEmail();
-                    client.selectedEmail = null;
+                    int index = sendSelectedEmailToTrash(client.sent);
+                    showNextEmail(index);
                 }
+
                 case RECEIVED -> {
-                    client.inbox.remove(client.selectedEmail);
-                    client.trash.add(client.selectedEmail);
-                    resetSelectedEmail();
-                    client.selectedEmail = null;
+                    int index = sendSelectedEmailToTrash(client.inbox);
+                    showNextEmail(index);
                 }
-                //TODO add trashed section
+
+                case TRASHED -> {
+                    int index = sendSelectedEmailToTrash(client.trash);
+                    showNextEmail(index);
+                }
+
             }
 
+        }
+    }
+
+    private int sendSelectedEmailToTrash(ObservableList<Email> list)
+    {
+        //TODO solve out of bounds
+        int ret = list.indexOf(client.selectedEmail);
+
+        if(client.selectedEmail.state != EmailState.TRASHED)
+        {
+            client.selectedEmail.state = EmailState.TRASHED;
+            client.trash.add(client.selectedEmail);
+        }
+        list.remove(client.selectedEmail);
+        resetSelectedEmail();
+
+        return ret;
+    }
+
+    private void showNextEmail(int indexOfEmailToBeShown)
+    {
+        if (indexOfEmailToBeShown > 0 && indexOfEmailToBeShown < emailListView.getItems().size())
+        {
+            emailListView.getSelectionModel().select(indexOfEmailToBeShown - 1);
+            client.selectedEmail = emailListView.getItems().get(indexOfEmailToBeShown - 1);
+            bindMailToView(client.selectedEmail);
         }
     }
 
@@ -115,6 +146,7 @@ public class ClientController {
         subjectTextField.setText("");
         selectedEmailView.textProperty().unbind();
         selectedEmailView.setText("");
+
     }
 
 }
--- MailClientServer/src/main/java/com/progiii/mailclientserver/client/view/ClientView.java
+++ MailClientServer/src/main/java/com/progiii/mailclientserver/client/view/ClientView.java
@@ -1,6 +1,5 @@
 package com.progiii.mailclientserver.client.view;
 
-import com.progiii.mailclientserver.client.controller.ClientController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
